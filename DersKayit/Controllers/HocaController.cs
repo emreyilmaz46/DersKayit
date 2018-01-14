ac154ba4ddf8446ff638569336720359e2aa73d1
EmreYZ <emreyilmaz46@gmail.com>
2018-01-14 21:55:03 +0300

    Öğretim elemanı ders işlemleri sayfası; küçük bir ara yüz düzenlemesi yapıldı

--- DersKayit/Controllers/HocaController.cs
+++ DersKayit/Controllers/HocaController.cs
@@ -1,31 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using DersKayit.Models;
-
-//namespace DersKayit.Controllers
-//{
-//    public class HocaController : Controller
-//    {
-//        DersKayitContext Db = new DersKayitContext();
-//        // GET: Hoca
-//        [HttpPost]
-//        public ActionResult Index(int HocaNo)
-//        {
-//            Hoca tempHoca = new Hoca();
-//            tempHoca = Db.Hocalar.SingleOrDefault(I => I.HocaNo == HocaNo);
-//            List<Ders> HocaninDersleri = new List<Ders>();
-//            HocaninDersleri = tempHoca.Dersler.ToList();
-
-//            return View(HocaninDersleri);
-//        }
-//    }
-//}
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
